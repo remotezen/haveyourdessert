5510867daf1d0f2fef95d9336fc39cfdd5b25198
Fred Hudson <remotezen@bell.net>
2015-03-08 12:50:46 -0400

    added capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,56 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+
+set :application, 'haveyourdessert.com'
+set :repo_url, 'git@example.com:remotezen/haveyourdessert.git'
+set :branch, 'master'
+set :server,  167.114.153.5
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app_name
+ set :deploy_to, '/var/www/haveyourdessert'
+
+# Default value for :scm is :git
+ set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+ set :log_level, :debug
+
+# Default value for :pty is false
+ set :pty, true
+
+# Default value for :linked_files is []
+
+ set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :rb_type :system
+set :rbenv_ruby, '2.1.5'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+ set :keep_releases, 5
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
